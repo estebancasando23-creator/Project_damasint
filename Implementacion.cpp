c68ecc0bcc82641614cec6b4408db1bb5523eaa4
estebancasando23-creator <estebancasando23@gmail.com>
2025-11-08 20:41:56 -0500

    Update Ingresarnombres function for player input
    Fix function to read player names and convert to uppercase.

--- Implementacion.cpp
+++ Implementacion.cpp
@@ -1,1 +1,15 @@
 //implementacion 
+
+#include "interfaz.h"
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+
+void  Ingresarnombres(char* nombre1, char* nombre2){
+    std::cout<<"ingrese el nombre del jugador 1: ";
+    std::cin>>nom1;
+    strupr(nom1);//esta es pa pasar a mayuscula  
+    std::cout<<"ingrese el nombre del jugador 2: ";
+    std::cin>>nom2;
+    strupr(nom2;)
+}
