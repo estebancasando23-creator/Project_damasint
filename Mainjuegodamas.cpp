2fb501437628d7aa836f0cf59c9899ed3ba4c1c6
estebancasando23-creator <estebancasando23@gmail.com>
2025-11-09 20:28:25 -0500

    Initialize game with player names and turn logic
    Added player names input and game initialization.

--- Mainjuegodamas.cpp
+++ Mainjuegodamas.cpp
@@ -1,5 +1,13 @@
 int main(){
+    char fichaJ1;
+    char fichaJ2;
+    int Turno;
+    char* nombreJ1 = new char[20];
+    char* nombreJ2 = new char[20];
 
-
-    return;
-}
+    int puntajeJ1, puntajeJ2;
+    int movientosJ1 = 0, movimientosJ2 = 0;
+    
+    Ingresarnombres(nombreJ1, nombreJ2);
+    Lanzardado(puntajeJ1, puntajeJ2, nombreJ1, nombreJ2);
+    Elegirficha(fichaJ1, fichaJ2, nombreJ1, nombreJ2, puntajeJ1, puntajeJ2, Turno);
