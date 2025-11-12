98d95cc2d9e5be194be157e8ee3080b57c55fe63
MrmasterNico <juanicolasbuitragogarzo@gmail.com>
2025-11-11 23:32:13 -0500

    Refactor game initialization and menu display

--- Mainjuegodamas.cpp
+++ Mainjuegodamas.cpp
@@ -1,38 +1,37 @@
 int main() {
+    std::srand((unsigned)std::time(nullptr));
+
+    std::string jugador1, jugador2;
+    int dado1 = 0, dado2 = 0, turno = 0;
+    char ficha1 = ' ', ficha2 = ' ';
     int opcion = 0;
 
     do {
         system("cls");
-        std::cout << "===== MENU =====\n";
+        SetColorSimple(FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+        std::cout << "=============================\n";
+        std::cout << "       MENU PRINCIPAL        \n";
+        std::cout << "=============================\n";
         std::cout << "1. Jugar\n";
         std::cout << "0. Salir\n";
-        std::cout << "================\n";
+        std::cout << "=============================\n";
         std::cout << "Seleccione una opcion: ";
         std::cin >> opcion;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
         switch (opcion) {
-            case 1: {
+            case 1:
                 system("cls");
                 std::cout << "Iniciando el juego...\n";
-
-                // Aquí llamas tus funciones del juego
-                char fichaJ1, fichaJ2;
-                int turno = 0;
-                int dado1 = 0, dado2 = 0;
-                char nombreJ1[20];
-                char nombreJ2[20];
-
-                Ingresarnombres(nombreJ1, nombreJ2);
-                Lanzardado(dado1, dado2, nombreJ1, nombreJ2);
-                Elegirficha(fichaJ1, fichaJ2, nombreJ1, nombreJ2, dado1, dado2, turno);
-
-                std::cout << "\nJuego finalizado.\n";
+                Ingresarnombres(jugador1, jugador2);
+                Lanzardado(dado1, dado2, jugador1, jugador2);
+                ElegirFicha(ficha1, ficha2, jugador1, jugador2, dado1, dado2, turno);
+                std::cout << "\n¡Juego finalizado!\n";
                 system("pause");
                 break;
-            }
 
             case 0:
-                std::cout << "Saliendo...\n";
+                std::cout << "Saliendo del programa...\n";
                 break;
 
             default:
